1653d00f88dc8d5c11e11835c152f21c4f6d6f96
matherbs <32384824+matherbs@users.noreply.github.com>
2017-11-13 15:00:20 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+
+    <meta charset="utf-8">
+    <title>Todo_app</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+
+  </head>
+
+  <body>
+
+    <!-- MAIN START -->
+    <div class="main">
+
+      <!-- MAIN-HEADER START -->
+      <div class="main-header">
+        <h1>MY TODOLIST</h1>
+        <img src="./assets/logo/plus-button.svg" alt="add">
+      </div>
+      <!-- MAIN-HEADER END -->
+
+      <!-- MAIN.CONTAINER START -->
+      <div class="main-container">
+        <ul class="list" id="todo-list">
+
+          <li class="list-item">
+
+            <!-- LI HEADER START -->
+            <header class="item-header">
+              <h2 class="item-title">My title</h2>
+              <ul class="menu">
+                <li class="menu-item" id="delete">Edit</li>
+                <li class="menu-item" id="edit">Delete</li>
+              </ul>
+            </header>
+            <!-- LI HEADER END -->
+
+            <!-- LI MAIN START -->
+            <div class="item-container">
+              <p>My description</p>
+            </div>
+            <!-- LI MAIN END -->
+
+            <!-- LI FOOTER START -->
+            <footer class="item-footer">
+              <span>Start at...</span>
+              <span>End at...</span>
+            </footer>
+            <hr class="hr-item-footer">
+            <!-- LI FOOTER END -->
+
+          </li>
+
+          <li class="list-item">
+
+            <!-- LI HEADER START -->
+            <header class="item-header">
+              <h2 class="item-title">My title</h2>
+              <ul class="menu">
+                <li class="menu-item" id="delete">Edit</li>
+                <li class="menu-item" id="edit">Delete</li>
+              </ul>
+            </header>
+            <!-- LI HEADER END -->
+
+            <!-- LI MAIN START -->
+            <div class="item-container">
+              <p>My description</p>
+            </div>
+            <!-- LI MAIN END -->
+
+            <!-- LI FOOTER START -->
+            <footer class="item-footer">
+              <span>Start at...</span>
+              <span>End at...</span>
+            </footer>
+            <hr class="hr-item-footer">
+            <!-- LI FOOTER END -->
+
+          </li>
+
+          <li class="list-item">
+
+            <!-- LI HEADER START -->
+            <header class="item-header">
+              <h2 class="item-title">My title</h2>
+              <ul class="menu">
+                <li class="menu-item" id="delete">Edit</li>
+                <li class="menu-item" id="edit">Delete</li>
+              </ul>
+            </header>
+            <!-- LI HEADER END -->
+
+            <!-- LI MAIN START -->
+            <div class="item-container">
+              <p>My description</p>
+            </div>
+            <!-- LI MAIN END -->
+
+            <!-- LI FOOTER START -->
+            <footer class="item-footer">
+              <span>Start at...</span>
+              <span>End at...</span>
+            </footer>
+            <hr class="hr-item-footer">
+            <!-- LI FOOTER END -->
+
+          </li>
+
+          <li class="list-item">
+
+            <!-- LI HEADER START -->
+            <header class="item-header">
+              <h2 class="item-title">My title</h2>
+              <ul class="menu">
+                <li class="menu-item" id="delete">Edit</li>
+                <li class="menu-item" id="edit">Delete</li>
+              </ul>
+            </header>
+            <!-- LI HEADER END -->
+
+            <!-- LI MAIN START -->
+            <div class="item-container">
+              <p>My description</p>
+            </div>
+            <!-- LI MAIN END -->
+
+            <!-- LI FOOTER START -->
+            <footer class="item-footer">
+              <span>Start at...</span>
+              <span>End at...</span>
+            </footer>
+            <hr class="hr-item-footer">
+            <!-- LI FOOTER END -->
+
+          </li>
+
+          <li class="list-item">
+
+            <!-- LI HEADER START -->
+            <header class="item-header">
+              <h2 class="item-title">My title</h2>
+              <ul class="menu">
+                <li class="menu-item" id="delete">Edit</li>
+                <li class="menu-item" id="edit">Delete</li>
+              </ul>
+            </header>
+            <!-- LI HEADER END -->
+
+            <!-- LI MAIN START -->
+            <div class="item-container">
+              <p>My description</p>
+            </div>
+            <!-- LI MAIN END -->
+
+            <!-- LI FOOTER START -->
+            <footer class="item-footer">
+              <span>Start at...</span>
+              <span>End at...</span>
+            </footer>
+            <hr class="hr-item-footer">
+            <!-- LI FOOTER END -->
+
+          </li>
+
+          <li class="list-item">
+
+            <!-- LI HEADER START -->
+            <header class="item-header">
+              <h2 class="item-title">My title</h2>
+              <ul class="menu">
+                <li class="menu-item" id="delete">Edit</li>
+                <li class="menu-item" id="edit">Delete</li>
+              </ul>
+            </header>
+            <!-- LI HEADER END -->
+
+            <!-- LI MAIN START -->
+            <div class="item-container">
+              <p>My description</p>
+            </div>
+            <!-- LI MAIN END -->
+
+            <!-- LI FOOTER START -->
+            <footer class="item-footer">
+              <span>Start at...</span>
+              <span>End at...</span>
+            </footer>
+            <hr class="hr-item-footer">
+            <!-- LI FOOTER END -->
+
+          </li>
+        </ul>
+
+      </div>
+      <!-- MAIN.CONTAINER END -->
+
+      <!-- NEXT.CONTAINER START -->
+      <div class="next-container">
+
+      </div>
+      <!-- NEXT.CONTAINER END -->
+
+      <!-- MAIN-FOOTER START -->
+      <div class="main-footer">
+        <p id="show"> Show: <span id="all">All task</span> <span id="todo">Todo task</span> <span id="done">Done task</span> </p>
+      </div>
+      <!-- MAIN-FOOTER END -->
+
+    </div>
+    <!-- MAIN END -->
+
+  </body>
+</html>
