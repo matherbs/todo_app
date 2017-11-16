f76dc413c63a1a7519af8a93e3e3b634fed87dbf
matherbs <32384824+matherbs@users.noreply.github.com>
2017-11-16 09:49:28 +0100

    16-11

--- todo.js
+++ todo.js
@@ -1,3 +1,8 @@
+
+//////////////////
+// SWITCH PAGES //
+//////////////////
+
 let switcher = document.getElementById("main-header-right");
 
 let page1 = document.getElementById("main-container-show");
@@ -9,4 +14,16 @@ function addTask() {
 
   page1.classList.toggle("hide");
   page2.classList.toggle("hide");
+};
+
+/////////////////
+// CLEAR INPUT //
+/////////////////
+
+let inputTitle = document.getElementById("input-title");
+let inputDescription = document.getElementById("input-description");
+
+function clearButton() {
+  inputTitle.value = " ";
+  inputDescription.value = " ";
 }
